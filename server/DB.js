dd61adc8949c0e3a3a4fc1b022aa294292e9a352
Jai Sharma <51614370+JaiSharma1710@users.noreply.github.com>
2025-03-24 21:26:13 +0530

    Development (#2)
    ---------
    
    Co-authored-by: Jai Sharma <jai.sharma@universityliving.com>

---
+++ server/DB.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+
+async function DB(selectedDB) {
+  const DB_LIST = process.env.DB_LIST;
+  const DB_NAME = process.env.DB_NAME;
+  const SELECTED_DB_URL = JSON.parse(DB_LIST)[selectedDB];
+  const SELECTED_DB_NAME = JSON.parse(DB_NAME)[selectedDB];
+
+  if (!SELECTED_DB_URL) {
+    return { db: '', client: '', error: true };
+  }
+
+  const client = new MongoClient(SELECTED_DB_URL);
+  await client.connect().catch(console.error);
+  console.log('Connected successfully to DB');
+  const db = client.db(SELECTED_DB_NAME);
+
+  return { db, client, error: false };
+}
+
+module.exports = DB;
